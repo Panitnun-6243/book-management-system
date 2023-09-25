9957101833629f4d6855720d2623b05667885336
Panitnun-6243 <panitnun.6243@gmail.com>
2023-09-25 19:47:09 +0700

    feat: add basic operation

--- index.js
+++ index.js
@@ -1,19 +1,68 @@
+// array of books
 let library = [];
 
+// create book object
+const book = new Object();
+
+// basic function
 const addBook = () => {
   console.log("เพิ่มหนังสือ");
+  book.title = prompt("ชื่อหนังสือ");
+  book.author = prompt("ผู้เขียน");
+  book.year = prompt("ปีที่พิมพ์");
+  book.price = prompt("ราคา");
+  library.push(book);
 };
 
 const viewBooks = () => {
   console.log("ดูรายการหนังสือ");
+  alert("รายการหนังสือ:\n\n" + JSON.stringify(library, null, 2));
 };
 
 const editBook = () => {
   console.log("แก้ไขหนังสือ");
+  let index = prompt("เลือกหนังสือที่ต้องการแก้ไข (ใส่เลขลำดับ):");
+  if (index >= 0 && index < books.length) {
+    let updatedBook = {
+      title: prompt("ชื่อหนังสือ:", books[index].title),
+      author: prompt("ผู้เขียน:", books[index].author),
+      year: prompt("ปีที่พิมพ์:", books[index].year),
+      price: prompt("ราคา:", books[index].price),
+    };
+
+    // check if all fields are filled
+    if (
+      updatedBook.title &&
+      updatedBook.author &&
+      updatedBook.year &&
+      updatedBook.price
+    ) {
+      library[index] = updatedBook;
+      alert("แก้ไขหนังสือเรียบร้อยแล้ว");
+    } else {
+      alert("กรุณากรอกข้อมูลให้ครบถ้วน");
+    }
+  } else {
+    alert("เลขลำดับไม่ถูกต้อง");
+  }
 };
 
 const deleteBook = () => {
   console.log("ลบหนังสือ");
+  let index = prompt("เลือกหนังสือที่ต้องการลบ (ใส่เลขลำดับ):");
+  if (index >= 0 && index < library.length) {
+    let confirmDelete = prompt(
+      "ต้องการลบหนังสือ " +
+        library[index].title +
+        " ใช่หรือไม่ (พิมพ์ yes เพื่อยืนยันการลบ):"
+    );
+    if (confirmDelete.toLowerCase() == "yes") {
+      library.splice(index, 1);
+      alert("ลบหนังสือเรียบร้อยแล้ว");
+    }
+  } else {
+    alert("เลขลำดับไม่ถูกต้อง");
+  }
 };
 
 const main = () => {
